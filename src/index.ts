79f93c7d235c33af5b615536171f893772f65c7f
Cunha-Renato <cunhamrenato@gmail.com>
2026-04-02 19:58:14 -0300

    Option.From

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,15 @@ export class Option<T> {
 
 	/**
 	 *
+	 * @param value
+	 * @returns `Option<T>` where `Some(value)` if `value` !== null and `None` if `value` === null.
+	 */
+	static From<T>(value: T | null): Option<T> {
+		return value === null ? Option.None() : Option.Some(value);
+	}
+
+	/**
+	 *
 	 * @param value - The value to be `Some`.
 	 * @returns Option<T> with `Some` = `value`.
 	 * @example
